d41982b3e22737f2b1e0446bed2b623affc2c5cf
이수영 <dannylisa67@gmail.com>
2022-07-20 11:37:34 +0900

    configs setup

--- dist/src/dateFormatter.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function dateFormatter(date: Date, pattern: string): string;
-//# sourceMappingURL=dateFormatter.d.ts.map
--- dist/src/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { LogInterceptor } from "./interceptor";
-export * from "./interceptor";
-export * from "./types";
-declare const interceptor: LogInterceptor;
-export default interceptor;
-//# sourceMappingURL=index.d.ts.map
